a6c123f3e64abbabee9313f22429fb5d65a2a900
Elinforelin <alina.m@gowombat.team>
2023-01-06 20:08:21 +0200

    features: add new

--- src/pages/WelcomePage/index.tsx
+++ src/pages/WelcomePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Button from "@mui/material/Button";
 
 import styles from "./styles.module.scss";
@@ -6,20 +6,34 @@ import { useNavigate } from "react-router-dom";
 import { SET_TEAMS } from "../../constants/routes";
 
 const WelcomePage: FC = () => {
+  const [animationActive, setAnimationActive] = useState(false);
   const navigate = useNavigate();
 
-  const onGoToSettingsPage = () => navigate(SET_TEAMS);
+  const onGoToSettingsPage = () => {
+    setAnimationActive(true);
+    setTimeout(() => navigate(SET_TEAMS), 2000);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.logoContainer}>
-        <h1>The Hat</h1>
+        <h1 className={animationActive ? styles.animatedTitle : ""}>The Hat</h1>
       </div>
       <div className={styles.buttonContainer}>
-        <Button onClick={onGoToSettingsPage} variant="contained">
+        <Button
+          className={animationActive ? styles.btn1 : ""}
+          onClick={onGoToSettingsPage}
+          variant="contained"
+          color="primary"
+        >
           New game
         </Button>
-        <Button variant="contained">Rules</Button>
+        <Button
+          className={animationActive ? styles.btn2 : ""}
+          variant="contained"
+        >
+          Rules
+        </Button>
       </div>
     </div>
   );
